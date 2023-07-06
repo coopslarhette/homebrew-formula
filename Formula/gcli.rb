42279b251ba6790c2ab1de60e9acceab2a76a29d
coopslarhette <coopslarhette@gmail.com>
2023-07-06 15:46:39 -0700

    add

--- Formula/gcli.rb
+++ Formula/gcli.rb
@@ -2,7 +2,7 @@ class Gcli < Formula
   desc "gcli helps you do stuff, or remember how to do stuff, in the terminal"
   homepage "https://github.com/coopslarhette/gcli"
   url "https://github.com/coopslarhette/gcli/releases/download/0.0.1/gen-cli.tar.gz"
-  sha256 "0973a31ead9fb7ed31ea53d2d0923cb7cd16e29a54352673c9228493b9fe7f6f"
+  sha256 "8e8337535cb5053093ae07cf5fb1aa293750756b127381847f44aa091fb84b73"
   license "MIT"
   version "0.0.1"
 
