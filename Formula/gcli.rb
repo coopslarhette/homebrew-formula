676f235b1e998fc8a73a9d6ebe907f416c04d22b
Cooper LaRhette <coopslarhette@gmail.com>
2023-07-11 10:55:48 -0700

    Update gcli.rb

--- Formula/gcli.rb
+++ Formula/gcli.rb
@@ -1,8 +1,8 @@
 class Gcli < Formula
   desc "gcli helps you do stuff, or remember how to do stuff, in the terminal"
   homepage "https://github.com/coopslarhette/gcli"
-  url "https://github.com/coopslarhette/gcli/releases/download/0.0.1/gen-cli.tar.gz"
-  sha256 "8e8337535cb5053093ae07cf5fb1aa293750756b127381847f44aa091fb84b73"
+  url "https://github.com/coopslarhette/gcli/releases/download/0.0.1/gcli.tar.gz"
+  sha256 "f68b5ab248aa75c1bb69452cc66d05ca432319bde518f2514d58af091f3d58e3"
   license "MIT"
   version "0.0.1"
 
