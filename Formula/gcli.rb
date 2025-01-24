a95c45e110113cf5db7a1861422a7138d6824d78
Cooper LaRhette <coopslarhette@gmail.com>
2025-01-24 12:31:57 -0800

    add support for apple silicon

--- Formula/gcli.rb
+++ Formula/gcli.rb
@@ -1,12 +1,20 @@
 class Gcli < Formula
-  desc "gcli helps you do stuff, or remember how to do stuff, in the terminal"
-  homepage "https://github.com/coopslarhette/gcli"
-  url "https://github.com/coopslarhette/gcli/releases/download/0.0.1/gcli.tar.gz"
-  sha256 "f68b5ab248aa75c1bb69452cc66d05ca432319bde518f2514d58af091f3d58e3"
-  license "MIT"
-  version "0.0.1"
+ desc "gcli helps you do stuff, or remember how to do stuff, in the terminal"
+ homepage "https://github.com/coopslarhette/gcli"
+ version "0.0.2"
+ license "MIT"
 
-  def install
-    bin.install "gcli"
-  end
+ on_macos do
+   if Hardware::CPU.arm?
+     url "https://github.com/coopslarhette/gcli/releases/download/v0.0.2/gcli-arm64.tar.gz"
+     sha256 "d9699b4a97da3931e91a8198a5198dee6f4de95c5dbcd63032888f4e69de9fb7"
+   else
+     url "https://github.com/coopslarhette/gcli/releases/download/v0.0.2/gcli-x64.tar.gz"
+     sha256 "a7584e0157650e463ba9705606397a4e11d8cfb89935187a365235c7af87e236"
+   end
+ end
+
+ def install
+   bin.install "gcli"
+ end
 end
