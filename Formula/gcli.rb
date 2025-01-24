f8970908e24a2245cfa6f90081093235e35c3140
Cooper LaRhette <coopslarhette@gmail.com>
2025-01-24 12:55:48 -0800

    dynamic sha and release url

--- Formula/gcli.rb
+++ Formula/gcli.rb
@@ -1,21 +1,53 @@
 class Gcli < Formula
- desc "gcli helps you do stuff, or remember how to do stuff, in the terminal"
- homepage "https://github.com/coopslarhette/gcli"
- version "0.0.2"
- license "MIT"
+  desc "gcli helps you do stuff, or remember how to do stuff, in the terminal"
+  homepage "https://github.com/coopslarhette/gcli"
+  license "MIT"
 
- on_macos do
-   if Hardware::CPU.arm?
-     url "https://github.com/coopslarhette/gcli/releases/download/v0.0.2/gcli-arm64.tar.gz"
-     sha256 "d9699b4a97da3931e91a8198a5198dee6f4de95c5dbcd63032888f4e69de9fb7"
-   else
-     url "https://github.com/coopslarhette/gcli/releases/download/v0.0.2/gcli-x64.tar.gz"
-     sha256 "a7584e0157650e463ba9705606397a4e11d8cfb89935187a365235c7af87e236"
-   end
- end
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
 
- def install
-   bin.install "gcli-arm64" => "gcli" if Hardware::CPU.arm?
-   bin.install "gcli-x64" => "gcli" if Hardware::CPU.intel?
- end
+  on_macos do
+    if Hardware::CPU.arm?
+      url do
+        require 'net/http'
+        require 'json'
+        
+        latest_release = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/coopslarhette/gcli/releases/latest")))
+        asset = latest_release["assets"].find { |a| a["name"] == "gcli-arm64.tar.gz" }
+        asset["browser_download_url"]
+      end
+      sha256 do
+        require 'net/http'
+        require 'json'
+        
+        latest_release = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/coopslarhette/gcli/releases/latest")))
+        asset = latest_release["assets"].find { |a| a["name"] == "gcli-arm64.tar.gz" }
+        URI.open(asset["browser_download_url"]).then { |f| Digest::SHA256.file(f).hexdigest }
+      end
+    else
+      url do
+        require 'net/http'
+        require 'json'
+        
+        latest_release = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/coopslarhette/gcli/releases/latest")))
+        asset = latest_release["assets"].find { |a| a["name"] == "gcli-x64.tar.gz" }
+        asset["browser_download_url"]
+      end
+      sha256 do
+        require 'net/http'
+        require 'json'
+        
+        latest_release = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/coopslarhette/gcli/releases/latest")))
+        asset = latest_release["assets"].find { |a| a["name"] == "gcli-x64.tar.gz" }
+        URI.open(asset["browser_download_url"]).then { |f| Digest::SHA256.file(f).hexdigest }
+      end
+    end
+  end
+
+  def install
+    bin.install "gcli-arm64" => "gcli" if Hardware::CPU.arm?
+    bin.install "gcli-x64" => "gcli" if Hardware::CPU.intel?
+  end
 end
